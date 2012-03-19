763d19616d2b29885342a8e0db605426678fd70e
Ben Livingood <livingood@gmail.com>
2012-03-18 20:05:41 -0600

    Parse results from an nmap xml input

--- parse_nmap_xml.py
+++ parse_nmap_xml.py
@@ -1,20 +1,12 @@
 import urllib
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
 import sys
 
 #USAGE: sudo nmap -sP -PR -oX - localdomain.0/24 | ruby thisfile
-#RESULTS: ip_address , mac address
+#RESULTS: ip_address , mac address(optional)
 
-dom = minidom.parse(sys.stdin)
-for host in dom.getElementsByTagName('host'):
-     print host.toxml()
-##doc = Hpricot.XML(STDIN.readlines.join)
-
-##uplist = doc.search(:host).select do |h| 
-  #h.search("//status[@state='up']").length > 0 
- ## h.search("//status[@reason='arp-response']").length > 0 
-##end
-
-#each host has two addresses, the ip address and the mac
-
-##uplist.each {|h| puts (h/:address).map{ |i| i[:addr]}.join(" ")}
+doc = ET.parse(sys.stdin)
+uplist = [ ]
+for host in doc.findall('//host'):
+    uplist = uplist + [[ h.get('addr') for h in host if h.get('addr')  ]]
+print uplist
