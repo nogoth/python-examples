707507bfe6758c0ec2fa4c8601dc2e73f26d121c
Ben Livingood <livingood@gmail.com>
2012-03-18 20:11:19 -0600

    it is not ruby

--- parse_nmap_xml.py
+++ parse_nmap_xml.py
@@ -2,7 +2,7 @@ import urllib
 import xml.etree.ElementTree as ET
 import sys
 
-#USAGE: sudo nmap -sP -PR -oX - localdomain.0/24 | ruby thisfile
+#USAGE: sudo nmap -sP -PR -oX - localdomain.0/24 | python thisfile
 #RESULTS: ip_address , mac address(optional)
 
 doc = ET.parse(sys.stdin)
